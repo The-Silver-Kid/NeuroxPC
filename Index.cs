2ae8c96784407a7a014e0d3d4ee15c5d73c3a3ce
Reggie Peck <The-Silver-Kid@users.noreply.github.com>
2018-03-20 12:52:38 -0600

    Initial commit

---
+++ Index.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace NeuroxPC {
+    class Index {
+        Dictionary<uint, string> pages = new Dictionary<uint, string>();
+        byte magicNumber = 0x0;
+        byte[] hash;
+        public bool checkout = false;
+
+        public Index(byte magic, string key) {
+            magicNumber = magic;
+            if (key.Length > 8)
+                key = key.Substring(0, 8);
+            if (key.Length < 8)
+                do {
+                    key = key + "^";
+                } while (key.Length < 8);
+            hash = SHA512.Create().ComputeHash(Encoding.Unicode.GetBytes(key));
+            for (int i = 0; i < hash.Length; i++)
+                hash[i] = (byte)(hash[i] ^ magicNumber);
+            pages.Clear();
+            checkout = true;
+        }
+
+        public Index(byte[] data, byte magic, string key) {
+            if (key.Length > 8)
+                key = key.Substring(0, 8);
+            if (key.Length < 8)
+                do {
+                    key = key + "^";
+                } while (key.Length < 8);
+            hash = SHA512.Create().ComputeHash(Encoding.Unicode.GetBytes(key));
+            for (int i = 0; i < hash.Length; i++)
+                hash[i] = (byte)(hash[i] ^ magic);
+            pages.Clear();
+            checkout = true;
+            for (int i = 0; i < hash.Length; i++)
+                if (hash[i] != data[i])
+                    checkout = false;
+
+            byte[] pagesD = new byte[data.Length - hash.Length];
+            for (int i = 0; i < pagesD.Length; i++)
+                pagesD[i] = data[i + hash.Length];
+            Array.Reverse(pagesD);
+
+            int Zelgius = 0;
+            while (Zelgius < pagesD.Length) {
+                byte[] Eliwood = new byte[BitConverter.ToUInt16(pagesD, Zelgius)];
+                Zelgius += 2;
+                uint Lilina = BitConverter.ToUInt16(pagesD, Zelgius);
+                Zelgius += 2;
+
+                int hN = 0;
+                for (int i = 0; i < Eliwood.Length; i++) {
+                    Eliwood[i] = pagesD[Zelgius];
+                    Eliwood[i] = (byte)(Eliwood[i] ^ hash[hN]);
+                    hN++;
+                    if (hN == hash.Length)
+                        hN = 0;
+                    Zelgius++;
+                }
+
+                string Roy = Encoding.Unicode.GetString(Eliwood);
+                pages.Add(Lilina, Roy);
+            }
+        }
+
+        public byte[] encode() {
+            List<byte> Corrin = new List<byte>();
+            uint[] Niles = pages.Keys.ToArray();
+            string[] Beruka = pages.Values.ToArray();
+            for (int i = 0; i < pages.Count; i++) {
+                byte[] Arthur = Encoding.Unicode.GetBytes(Beruka[i]);
+                byte[] Percy = new byte[Arthur.Length + 4];
+                uint Nina = (uint) Arthur.Length;
+                Percy[0] = BitConverter.GetBytes(Nina)[0];
+                Percy[1] = BitConverter.GetBytes(Nina)[1];
+                Percy[2] = BitConverter.GetBytes(Niles[i])[0];
+                Percy[3] = BitConverter.GetBytes(Niles[i])[1];
+                int hN = 0;
+                for (int e = 0; e < Arthur.Length; e++) {
+                    Arthur[e] = (byte)(Arthur[e] ^ hash[hN]);
+                    hN++;
+                    if (hN == hash.Length)
+                        hN = 0;
+                    Percy[e + 4] = Arthur[e];
+                }
+                Corrin.AddRange(Percy);
+            }
+            Corrin.Reverse();
+            byte[] Kana = new Byte[Corrin.Count + hash.Length];
+            byte[] Felicia = Corrin.ToArray();
+            for (int i = 0; i < hash.Length; i++)
+                Kana[i] = hash[i];
+            for (int i = 0; i < Felicia.Length; i++)
+                Kana[i + hash.Length] = Felicia[i];
+            return Kana;
+        }
+
+        public void addPage(uint id, string data) {
+            pages.Add(id, data);
+        }
+
+        public void replacePage(uint id, string data) {
+            pages[id] = data;
+        }
+
+        public string getPage(uint id) {
+            return pages[id];
+        }
+
+        public uint[] getAllKeys() {
+            return pages.Keys.ToArray();
+        }
+
+        public bool contains(uint id) {
+            bool ret = false;
+            uint[] keys = pages.Keys.ToArray();
+            for (int i = 0; i < keys.Length; i++)
+                if (keys[i] == id)
+                    ret = true;
+            return ret;
+        }
+
+        public void deletePage(uint id) {
+            pages.Remove(id);
+        }
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuroxPC {
+    public partial class Window : Form {
+
+        Index indexedData;
+        Stream file;
+        uint page;
+        bool changes = false, fileChanged = false;
+
+        public Window() {
+            InitializeComponent();
+        }
+
+        private void form1_Load(object sender, EventArgs e) {
+            pageCount.Text = "Open or Create an Index File";
+        }
+
+        private void closeForm(object sender, FormClosingEventArgs e) {
+        }
+
+        private void createNewIndex(object sender, EventArgs e) {
+            saveBox.ShowDialog(this);
+        }
+
+        private void createNewIndexConfirm(object sender, CancelEventArgs e) {
+            file = saveBox.OpenFile();
+            debug.Text = "Creating...";
+            string pass = Prompt.getPass("Enter a password to protect this sheet", "Password");
+            byte pin = Prompt.getPin("Enter a PIN", "PIN");
+
+            indexedData = new Index(pin, pass);
+
+            byte[] crud = indexedData.encode();
+            file.Write(crud, 0, crud.Length);
+            file.Position = 0;
+
+            loadPage.Enabled = true;
+            newPage.Enabled = true;
+            delPage.Enabled = true;
+
+            debug.Text = "Created Index";
+            pageCount.Text = "Total Pages : 0";
+        }
+
+        private void loadContent(object sender, EventArgs e) {
+            openBox.ShowDialog(this);
+        }
+
+        private void loadConfirm(object sender, CancelEventArgs e) {
+            file = new FileStream(openBox.FileName, FileMode.Open, FileAccess.ReadWrite);
+            debug.Text = "Loading...";
+            string pass = Prompt.getPass("Enter the password", "Password");
+            byte pin = Prompt.getPin("Enter the PIN", "PIN");
+
+            byte[] crud = new byte[file.Length];
+            file.Read(crud, 0, crud.Length);
+            file.Position = 0;
+
+            indexedData = new Index(crud, pin, pass);
+
+            loadPage.Enabled = true;
+            newPage.Enabled = indexedData.checkout;
+            delPage.Enabled = indexedData.checkout;
+
+            if (!(indexedData.getAllKeys().Length == 0)) {
+                pageNumber.Enabled = true;
+                pageNumber.DataSource = indexedData.getAllKeys();
+            }
+            debug.Text = "Loaded Index";
+            pageCount.Text = "Total Pages : " + indexedData.getAllKeys().Length;
+        }
+
+        private void cNewPage(object sender, EventArgs e) {
+            data.Enabled = true;
+            data.ResetText();
+            page = getNextPage();
+            debug.Text = "Currently Editing page : " + page;
+        }
+
+        private uint getNextPage() {
+            uint ret = 0;
+            uint[] keys = indexedData.getAllKeys();
+            for (int i = 0; i < keys.Length; i++)
+                if (ret < keys[i])
+                    ret = keys[i];
+            ret++;
+            return ret;
+        }
+
+        private void lPage(object sender, EventArgs e) {
+            if (pageNumber.Enabled) {
+                data.Enabled = true;
+                data.ResetText();
+                data.Text = indexedData.getPage((uint)pageNumber.SelectedValue);
+                debug.Text = "Loaded Page : " + pageNumber.SelectedValue;
+            }
+        }
+
+        private void saveLocal(object sender, EventArgs e) {
+            savePage.Enabled = false;
+            debug.Text = debug.Text.Replace("\nUnsaved Changes!", string.Empty);
+            if (indexedData.contains(page))
+                indexedData.replacePage(page, data.Text);
+            else
+                indexedData.addPage(page, data.Text);
+            fileChanged = true;
+            writeChanges.Enabled = true;
+            if (!(indexedData.getAllKeys().Length == 0)) {
+                pageNumber.Enabled = true;
+                pageNumber.DataSource = indexedData.getAllKeys();
+            }
+            changes = false;
+            pageCount.Text = "Total Pages : " + indexedData.getAllKeys().Length;
+            debug.Text = "Saved Changes Internally. Write to file to make changes official.";
+        }
+
+        private void delPageB(object sender, EventArgs e) {
+            if (pageNumber.Enabled) {
+                debug.Text = "Deleted Page : " + pageNumber.SelectedValue;
+                if (!(indexedData.getAllKeys().Length == 0)) {
+                    pageNumber.Enabled = true;
+                    pageNumber.DataSource = indexedData.getAllKeys();
+                } else {
+                    pageNumber.Enabled = false;
+                }
+                pageCount.Text = "Total Pages : " + indexedData.getAllKeys().Length;
+            }
+        }
+
+        private void writeChangesB(object sender, EventArgs e) {
+            if (indexedData.checkout) {
+                writeChanges.Enabled = false;
+                byte[] crud = indexedData.encode();
+                file.Write(crud, 0, crud.Length);
+                file.Position = 0;
+                debug.Text = "Wrote Changes to File.";
+            } else
+                debug.Text = "Cannot Write to File becasue of incorrect password or pin...";
+        }
+
+        private void data_TextChanged(object sender, EventArgs e) {
+            if (!changes) {
+                changes = true;
+                savePage.Enabled = true;
+                debug.Text = debug.Text + "\nUnsaved Changes!";
+            }
+        }
+    }
+
+    static class Prompt {
+        public static string getPass(string text, string caption) {
+            Form prompt = new Form() {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Width = 400, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            textBox.MaxLength = 8;
+            Button confirmation = new Button() { Text = "Alright!", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+
+        public static byte getPin(string text, string caption) {
+            Form prompt = new Form() {
+                Width = 250,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Width = 100, Text = text };
+            NumericUpDown textBox = new NumericUpDown() { Left = 50, Top = 50, Width = 100 };
+            textBox.Maximum = Byte.MaxValue;
+            textBox.Minimum = Byte.MinValue;
+            Button confirmation = new Button() { Text = "Excellent!", Left = 50, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? (byte)textBox.Value : (byte)0;
+        }
+    }
+}
